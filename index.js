0649778f438b703d221e68f1d2be11b6fadb769f
Lily Chisholm <lsc112003@gmail.com>
2021-05-27 04:12:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,22 +10,10 @@ function logShout(string) {
 function logWhisper(string) {
   return console.log('hello')
 }
-var uppercase = "HELLO!"
 
-uppercase.toUpperCase() === uppercase // true
-
-var lowercase = 'hello!'
-
-lowercase.toLowerCase() === lowercase // true
-
-var mixedCase = 'I love you, Grandma.'
-
-mixedCase.toLowerCase() === mixedCase // false
-
-mixedCase.toUpperCase() === mixedCase
 
 function sayHiToGrandma(string){
-  if (string === lowercase) {
+  if (string === string.toLowerCase) {
     return console.log ("I can\'t hear you!")
   }
 }
